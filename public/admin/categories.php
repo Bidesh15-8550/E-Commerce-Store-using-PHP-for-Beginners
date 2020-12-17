2bbc28d781326a2f86231bc77023005d92f4be86
Bidesh15-8550 <bideshbiswas@ymail.com>
2020-12-17 17:46:36 +0600

    errors

--- public/admin/categories.php
+++ public/admin/categories.php
@@ -65,20 +65,20 @@
             <div class="collapse navbar-collapse navbar-ex1-collapse">
                 <ul class="nav navbar-nav side-nav">
                     <li class="active">
-                        <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+                        <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
                     </li>
                     <li>
-                        <a href="charts.html"><i class="fa fa-fw fa-bar-chart-o"></i> View Products</a>
+                        <a href="charts.php"><i class="fa fa-fw fa-bar-chart-o"></i> View Products</a>
                     </li>
                     <li>
-                        <a href="tables.html"><i class="fa fa-fw fa-table"></i> Add Product</a>
+                        <a href="tables.php"><i class="fa fa-fw fa-table"></i> Add Product</a>
                     </li>
                     
                     <li>
-                        <a href="bootstrap-elements.html"><i class="fa fa-fw fa-desktop"></i> Categories</a>
+                        <a href="bootstrap-elements.php"><i class="fa fa-fw fa-desktop"></i> Categories</a>
                     </li>
                     <li>
-                        <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i>Orders</a>
+                        <a href="bootstrap-grid.php"><i class="fa fa-fw fa-wrench"></i>Orders</a>
                     </li>
                 
                 </ul>
