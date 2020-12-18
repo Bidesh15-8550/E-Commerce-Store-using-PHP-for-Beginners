29517a8d2e4af4b3a796b2c527e2f7605c1585ce
Bidesh15-8550 <bideshbiswas@ymail.com>
2020-12-18 18:28:51 +0600

    errors

--- public/admin/users.php
+++ public/admin/users.php
@@ -65,10 +65,10 @@
             <div class="collapse navbar-collapse navbar-ex1-collapse">
                 <ul class="nav navbar-nav side-nav">
                     <li class="active">
-                        <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+                        <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
                     </li>
                     <li>
-                        <a href="charts.html"><i class="fa fa-fw fa-bar-chart-o"></i> View Products</a>
+                        <a href="charts.php"><i class="fa fa-fw fa-bar-chart-o"></i> View Products</a>
                     </li>
                     <li>
                         <a href="tables.html"><i class="fa fa-fw fa-table"></i> Add Product</a>
