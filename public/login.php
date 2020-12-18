1b3c1a0e506424e1157a45895fe80f845df125f5
Bidesh15-8550 <bideshbiswas@ymail.com>
2020-12-18 18:28:32 +0600

    errors

--- public/login.php
+++ public/login.php
@@ -104,7 +104,7 @@
         <footer>
             <div class="row">
                 <div class="col-lg-12">
-                    <p>Copyright &copy; Your Website 2030</p>
+                    <p>Copyright &copy; Your Website 2020</p>
                 </div>
             </div>
         </footer>
