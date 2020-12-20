a1037dda49b485ee0c7555029b8d5a789c0ad6bd
Bidesh15-8550 <bideshbiswas@ymail.com>
2020-12-20 18:24:40 +0600

    errors solved

--- public/admin/index.php
+++ public/admin/index.php
@@ -65,10 +65,10 @@
             <div class="collapse navbar-collapse navbar-ex1-collapse">
                 <ul class="nav navbar-nav side-nav">
                     <li class="active">
-                        <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+                        <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
                     </li>
                     <li>
-                        <a href="products.html"><i class="fa fa-fw fa-bar-chart-o"></i> View Products</a>
+                        <a href="products.php"><i class="fa fa-fw fa-bar-chart-o"></i> View Products</a>
                     </li>
                     <li>
                         <a href="add_product.html"><i class="fa fa-fw fa-table"></i> Add Product</a>
